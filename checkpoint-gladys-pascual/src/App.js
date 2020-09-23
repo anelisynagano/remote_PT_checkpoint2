2c82909697b4c144704fac359cb194b76daa7a67
gladys-pascual <gladyskate@gmail.com>
2020-09-23 18:57:02 +0100

    fetch movies from API

--- checkpoint-gladys-pascual/src/App.js
+++ checkpoint-gladys-pascual/src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import MovieList from "./components/MovieList";
 
 const App = () => {
+  const [movie, setMovie] = useState({});
+
+  const getMovie = () => {
+    fetch(
+      "https://raw.githubusercontent.com/wildcodeschoolparis/datas/master/movies.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setMovie(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => getMovie(), []);
+
+  console.log(movie);
+
   return (
     <div>
-      <p>App</p>
-      <p>App</p>
+      <MovieList />
     </div>
   );
 };
